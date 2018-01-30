44aad59ed0d2ec149007b655e8e4f0abb973785c
SilasReinagel <silas.reinagel@gmail.com>
2018-01-29 23:57:25 -0700

    SQL Tasks WIP

---
+++ HomeTaskManagement.Sql/Tasks/TasksTable.cs
@@ -0,0 +1,59 @@
+﻿using HomeTaskManagement.App.Common;
+using HomeTaskManagement.App.Task;
+using System.Collections.Generic;
+
+namespace HomeTaskManagement.Sql.Tasks
+{
+    public sealed class TasksTable : IEntityStore<TaskRecord>
+    {
+        private readonly SqlDatabase _db;
+
+        public TasksTable(SqlDatabase db)
+        {
+            _db = db;
+        }
+
+        public TaskRecord Get(string id)
+        {
+            var sql = @"SELECT * 
+                        FROM HomeTask.Tasks
+                        WHERE Id = @id";
+
+            return _db.QuerySingle<TaskRecord>(sql, new { id });
+        }
+
+        public IEnumerable<TaskRecord> GetAll()
+        {
+            var sql = @"SELECT * 
+                        FROM HomeTask.Tasks";
+
+            return _db.Query<TaskRecord>(sql);
+        }
+
+        public void Put(string id, TaskRecord record)
+        {
+            var sql = @"UPDATE HomeTask.Tasks SET 
+                            Name = @name,
+                            UnitsOfWork = @unitsOfWork,
+                            Frequency = @frequency,
+                            Importance = @importance
+                        WHERE Id = @id;
+
+                        IF @@ROWCOUNT = 0
+                        BEGIN
+                            INSERT INTO HomeTask.Tasks (Id, Name, UnitsOfWork, Frequency, Importance)
+                            VALUES (@id, @name, @unitsOfWork, @importance)
+                        END";
+
+            _db.Execute(sql, record);
+        }
+
+        public void Remove(string id)
+        {
+            var sql = @"DELETE FROM HomeTask.Tasks 
+                        WHERE Id = @id";
+
+            _db.Execute(sql, new { id });
+        }
+    }
+}
---
+++ HomeTaskManagement.Sql/Tasks/TasksTableTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTaskManagement.Sql.Tasks
+{
+    [TestClass]
+    public sealed class TasksTableTests
+    {
+    }
+}
