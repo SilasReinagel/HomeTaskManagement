ac5935ad8b208f86d9695eab588998f8988d5014
SilasReinagel <silas.reinagel@gmail.com>
2018-02-10 15:35:57 -0700

    Client WIP

---
+++ HomeTaskManagement.UI/src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+import FlatButton from 'material-ui/FlatButton';
+
+import './App.css';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Session from "./Session";
+import CreateTask from "./CreateTask";
+injectTapEventPlugin();
+
+class App extends Component {  
+  constructor(props) {
+    super(props);
+    this.state={
+      menuOpen:false,
+      session:new Session(),
+      isLoggedIn:false,
+      view:[]
+    };
+  }
+  
+  openMenu = (_) => this.setState({menuOpen:!this.state.menuOpen}); 
+
+  componentWillMount() {
+    this.update();
+    if (!this.state.isLoggedIn) {
+      this.navigateTo("Login");
+    } else {
+      this.navigateTo("Dashboard");
+    }
+  }
+
+  navigateTo(viewName) {
+    console.log("Navigate to: " + viewName);
+    var view=[];
+    if(viewName === "Login") {
+      view.push(<Login appContext={this}/>);
+    } else if(viewName === "Dashboard") {
+      view.push(<Dashboard appContext={this}/>);
+    } else if(viewName === "Create Task") {
+      view.push(<CreateTask appContext={this}/>);
+    } else {
+      console.log("Unknown view: " + viewName);
+    }
+    this.setState({view:view, menuOpen:false});
+  }
+
+  render() {
+    return (
+      <div className="App">      
+        <MuiThemeProvider>
+          <div>
+            <AppBar title="Home Task Management" 
+              showMenuIconButton={this.state.isLoggedIn}
+              onLeftIconButtonClick={this.openMenu}
+              iconElementRight={this.state.isLoggedIn && <FlatButton label="Logout" onClick={(event) => this.logout()}/>}/>
+            <Drawer docked={false} open={this.state.menuOpen} onRequestChange={(open) => this.setState({menuOpen:open})}>
+              <MenuItem onClick={() => this.navigateTo("Create Task")}>Create Task</MenuItem>
+            </Drawer>
+            {this.state.view}
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+
+  update() {
+    this.state.isLoggedIn=this.state.session.isLoggedIn();
+  }
+
+  logout() {
+    this.state.session.logout();
+    this.update();
+    var loginView=[];
+    loginView.push(<Login appContext={this}/>);
+    this.setState({ view:loginView })
+  }
+}
+
+export default App;
---
+++ HomeTaskManagement.UI/src/Login.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+import Session from './Session';
+import Dashboard from './Dashboard';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      session:new Session(),
+      isBusy:false,
+      username:'',
+      password:''    
+    }
+  }
+
+  render() {
+    return (
+      <div>
+          <div>
+            <TextField hintText="Username" 
+              type="username" 
+              floatingLabelText="Username" 
+              onChange={(event, newValue) => this.setState({username:newValue})}/>
+            <br/>
+            <TextField hintText="Password" 
+              type="password" 
+              floatingLabelText="Password" 
+              onChange={(event, newValue) => this.setState({password:newValue})}
+              onKeyPress={(event) => this.handleButtonPress(event)} />
+            <br/>
+            {!this.isBusy && (<RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.login()}/>)}
+          </div>
+      </div>
+    );
+  }
+
+  handleButtonPress(event) {
+    if (event.key === 'Enter') {
+      this.login();
+      event.preventDefault();
+    }
+  }
+
+  login() {
+    var self = this;    
+    self.setState({isBusy:true});
+    var payload = {
+      "appname": "HomeTaskManagement",
+      "username": this.state.username,
+      "password": this.state.password
+    };
+
+    axios
+      .post("https://miniauth.azurewebsites.net/api/account/applogin", payload)
+      .then(function(response) {
+        console.log(response);
+        if(response.status === 200) {
+          console.log("Login successful");
+          self.state.session.setLoginInfo(self.state.username, response.data.Token, response.data.ExpiresAtUtc);
+
+          var dashboardView=[];
+          dashboardView.push(<Dashboard appContext={self.props.appContext}/>);
+          self.props.appContext.setState({view:dashboardView, isLoggedIn:true});
+        } else {
+          console.log("Login unsuccessful");
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+      self.setState({isBusy:false});
+  }
+}
+
+const style = {
+  margin: 30,
+};
+
+export default Login;
